6c5c38fb38c6fd52b8c4b59ad24cefb1e7bb11af
mohamedmbh <mohamedbenhamzaa@gmail.com>
2022-01-17 10:28:00 +0100

    Adds a method to get first multiplier of twenty from a number

--- SnakeGUI/src/utils/FrameUtils.java
+++ SnakeGUI/src/utils/FrameUtils.java
@@ -2,11 +2,11 @@ package utils;
 
 public class FrameUtils {
 
-	public static int getMultiplierOfTwentyFrom(int n) {
+	public static int getFirstMultiplierOfTwentyFrom(int n) {
 		int max = n;
 		int min = n - 20;
 
-		// Returns random number between a max and min
+		// Returns random number between a max and min multiplier of 20
 		return (int) Math.round((Math.random() * (max - min) + min) / 20) * 20;
 	}
 
