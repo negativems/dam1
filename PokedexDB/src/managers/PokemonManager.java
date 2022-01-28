90d5318074218789a3b69804efd3fd03aae1f11f
mohamedmbh <mohamedbenhamzaa@gmail.com>
2022-01-28 01:47:32 +0100

    Changes pokemon storage from locally to database

--- PokedexDB/src/managers/PokemonManager.java
+++ PokedexDB/src/managers/PokemonManager.java
@@ -3,26 +3,43 @@ package managers;
 import java.util.ArrayList;
 import java.util.List;
 
+import dao.PokemonDAO;
 import models.Gender;
 import models.Pokemon;
 
 public class PokemonManager {
 
-	private List<Pokemon> pokemons = new ArrayList<>();
+	private final PokemonDAO pokemonDAO;
+	private final List<Pokemon> pokemons;
 	
 	public PokemonManager() {
-		loadDefaultPokemons();
-	}
-	
-	private void loadDefaultPokemons() {
-		pokemons.add(new Pokemon(1, "Bulbasur", "Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.", "Semilla", "Espesura", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", 0.7F, 6.9F, Gender.MACHO));
-		pokemons.add(new Pokemon(2, "Ivysaur", "Cuando le crece bastante el bulbo del lomo, pierde la capacidad de erguirse sobre las patas traseras.", "Semilla", "Espesura", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png", 1.0F, 13.0F, Gender.MACHO));
-		pokemons.add(new Pokemon(3, "Venusaur", "La planta florece cuando absorbe energía solar, lo cual le obliga a buscar siempre la luz del sol.", "Semilla", "Espesura", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png", 2.0F, 100.0F, Gender.MACHO));
-		pokemons.add(new Pokemon(4, "Charmander", "Prefiere las cosas calientes. Dicen que cuando llueve le sale vapor de la punta de la cola.", "Fuego", "Lagartija", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", 2.0F, 100.0F, Gender.MACHO));
+		this.pokemonDAO = new PokemonDAO();
+		this.pokemons = pokemonDAO.getAllFromDatabase();
 	}
 	
 	public void addPokemon(Pokemon pokemon) {
 		pokemons.add(pokemon);
+	}
+	
+	public void savePokemons() {	
+		List<String[]> values = new ArrayList<>();
+		
+		for (Pokemon pokemon : pokemons) {
+			String id = pokemon.getId() + "";
+			String name = "'" + pokemon.getName() + "'";
+			String description = "'" + pokemon.getDescription() + "'";
+			String specie = "'" + pokemon.getSpecie() + "'";
+			String ability = "'" + pokemon.getAbility() + "'";
+			String imageURL = "'" + pokemon.getImageURL() + "'";
+			String height = pokemon.getHeight() + "";
+			String weight = pokemon.getWeight() + "";
+			String gender = pokemon.getGender() == Gender.MACHO ? "0" : "1";
+			String[] valuesArray = new String[] {id, name, description, specie, ability, imageURL, height, weight, gender};
+			values.add(valuesArray);
+		}
+		
+		pokemonDAO.delete("DELETE cFROM " + pokemonDAO.getTable());
+		pokemonDAO.insert("PokemonID, Name, Description, Specie, Ability, ImageURL, Height, Weight, Gender", values);
 	}
 	
 	// Getters & Setters
